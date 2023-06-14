fa914fdfc3064756f57e7a5900bbbb38438fddc9
NikaKlokava <veronika.kazakevich1997@gmail.com>
2023-06-14 17:44:13 +0300

    feat: add algorythm for filling cells

--- sudoku.ts
+++ sudoku.ts
@@ -46,19 +46,19 @@ function* chunks(arr, n) {
 }
 const data: FieldType = generateEmpty();
 
-[
-  [0, 0],
-  [1, 1],
-  [2, 2],
-].forEach(([i, j]) => {
-  const initial = Array.from(Array(9)).map((_, i) => i + 1);
-  shuffle(initial);
+// [
+//   [0, 0],
+//   [1, 1],
+//   [2, 2],
+// ].forEach(([i, j]) => {
+//   const initial = Array.from(Array(9)).map((_, i) => i + 1);
+//   shuffle(initial);
 
-  const chunked = [...chunks(initial, 3)];
-  chunked.forEach((ch, k) => {
-    data[i][j][k] = ch;
-  });
-});
+//   const chunked = [...chunks(initial, 3)];
+//   chunked.forEach((ch, k) => {
+//     data[i][j][k] = ch;
+//   });
+// });
 
 const getNumbersInRow = (index: number) => {
   const squareRow = Math.floor(index / 3);
@@ -83,29 +83,69 @@ const getNumbersInColumn = (index: number) => {
   }, new Array<number>());
 };
 
-[[0, 1]].forEach(([i, j]) => {
-  const initial = Array.from(Array(9)).map((_, i) => i + 1);
-  // shuffle(initial);
+const squares = [
+  [0, 0],
+  [1, 1],
+  [2, 2],
+  [0, 1],
+  [0, 2],
+  [1, 0],
+  [1, 2],
+  [2, 0],
+  [2, 1],
+];
+for (let sqIndex = 0; sqIndex < squares.length; sqIndex += 1) {
+  const [gRow, gColumn] = squares[sqIndex];
 
-  for (let h = 0; h < 3; h++) {
-    for (let g = 0; g < 3; g++) {
-      for (let k = 0; k < initial.length; k++) {
-        const numsInColumn = getNumbersInColumn(g + 3); // 3
-        const numbsInRow = getNumbersInRow(h); //0
+  let initial = Array.from(Array(9)).map((_, i) => i + 1);
+  shuffle(initial);
 
-        const isExistInRow = numbsInRow.includes(initial[k]);
-        const isExistInColumn = numsInColumn.includes(initial[k]);
+  const clear = () => {
+    Array.from(Array(3)).forEach((_, row) => {
+      Array.from(Array(3)).forEach((_, column) => {
+        data[gRow][gColumn][row][column] = 0;
+      });
+    });
+  };
 
-        if (!isExistInRow && !isExistInColumn) {
-          data[i][j][h][g] = initial[k];
-          const index = initial.indexOf(initial[k]);
-          initial.splice(index, 1);
-          break;
+  const tryToFill = () => {
+    Array.from(Array(3)).forEach((_, row) => {
+      Array.from(Array(3)).forEach((_, column) => {
+        const rowColumnValue = [
+          ...getNumbersInColumn(column + 3 * gColumn),
+          ...getNumbersInRow(row + 3 * gRow),
+        ];
+        const validValueIndex = initial.findIndex(
+          (num) => !rowColumnValue.includes(num)
+        );
+
+        if (validValueIndex === -1) {
+          throw new Error(
+            `Can find valid value for [gRow=${gRow}][gColumn=${gColumn}] [column=${column}][row=${row}].`
+          );
         }
-      }
+
+        data[gRow][gColumn][row][column] = initial[validValueIndex];
+        initial.splice(validValueIndex, 1);
+      });
+    });
+  };
+
+  const maxNumberOfAttempt = 10 * sqIndex + 1;
+  for (let attempt = 0; attempt < maxNumberOfAttempt; attempt += 1) {
+    try {
+      tryToFill();
+      break;
+    } catch (err) {
+      clear();
+      initial = Array.from(Array(9)).map((_, i) => i + 1);
+      shuffle(initial);
+    }
+    if (attempt === maxNumberOfAttempt - 1) {
+    //   console.warn("Start again");
+      sqIndex = 0;
     }
   }
-  return;
-});
+}
 
 printField(data);
