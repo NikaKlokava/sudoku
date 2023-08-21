b6d6eed4ae930d66d6e40282a0a5c0bd64e87c1b
NikaKlokava <veronika.kazakevich1997@gmail.com>
2023-08-21 13:05:21 +0300

    fix: fix links

--- src/shared/components/footer/Footer.tsx
+++ src/shared/components/footer/Footer.tsx
@@ -7,9 +7,15 @@ export const Footer = memo(() => {
       <p className={cl.footer_content}>Veranika Klokava</p>
       <p className={cl.footer_content}>2023</p>
       <ul className={cl.links}>
-        <li className={cl.instagram}></li>
-        <li className={cl.github}></li>
-        <li className={cl.linkedin}></li>
+        <a href="https://instagram.com/nika_klokava?igshid=MmIzYWVlNDQ5Yg==">
+          <div className={cl.instagram} />
+        </a>
+        <a href="https://github.com/NikaKlokava">
+          <div className={cl.github} />
+        </a>
+        <a href="https://linkedin.com/in/veranika-klokava-858b5b287">
+          <div className={cl.linkedin} />
+        </a>
       </ul>
     </footer>
   );
